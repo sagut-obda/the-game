90c574343ed346996ea9664e0ee8c12ef30af68e
Hayashi <gunawan.mr.blue@gmail.com>
2017-11-15 10:03:25 +0700

    adding main menu gui

---
+++ DinoTheGame/src/mygame/state/MainMenuGuiState.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.state;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.niftygui.NiftyJmeDisplay;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+/**
+ *
+ * @author Hayashi
+ */
+public class MainMenuGuiState extends SagutAppState implements ScreenController {
+    private Nifty nifty;
+    protected static MainMenuGuiState hud;
+    protected NiftyJmeDisplay niftyDisplay;
+
+    public static MainMenuGuiState getCurrentInstance() {
+        return hud;
+    }
+
+    public MainMenuGuiState(SimpleApplication sapp, String rootNodeName) {
+        super(sapp, rootNodeName);
+         hud = this;
+    }
+
+    @Override
+    protected void init(AppStateManager stateManager, Application app) {
+        niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
+                sapp.getAssetManager(), sapp.getInputManager(), sapp.getAudioRenderer(), sapp.getGuiViewPort());
+        nifty = niftyDisplay.getNifty();
+
+        nifty.fromXml("Interface/main-menu-gui.xml", "scrMainMenu", this);
+        sapp.getGuiViewPort().addProcessor(niftyDisplay);
+    }
+    
+    @Override
+    protected void cleanup(Application app) {
+        sapp.getGuiViewPort().removeProcessor(niftyDisplay);
+    }
+
+    @Override
+    public void bind(Nifty nifty, Screen screen) {
+    }
+
+    @Override
+    public void onStartScreen() {
+    }
+
+    @Override
+    public void onEndScreen() {
+    }
+    
+}
