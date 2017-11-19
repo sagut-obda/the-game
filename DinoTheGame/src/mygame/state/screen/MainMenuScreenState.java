494967c625e0d391166cdcb169405aafcb341760
ASUS-PC\ASUS <michaelstefanusvalentino@gmail.com>
2017-11-19 12:37:14 +0700

    Penambahan main menu

--- DinoTheGame/src/mygame/state/screen/MainMenuScreenState.java
+++ DinoTheGame/src/mygame/state/screen/MainMenuScreenState.java
@@ -11,8 +11,12 @@ import com.jme3.app.state.AppStateManager;
 import com.jme3.input.KeyInput;
 import com.jme3.input.controls.ActionListener;
 import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.AmbientLight;
+import com.jme3.math.ColorRGBA;
+import mygame.GameUtilities;
 import mygame.KeyBindings;
 import mygame.Main;
+import mygame.models.GameCharacter;
 import mygame.state.SagutAppState;
 import mygame.state.gui.MainMenuGuiState;
 
@@ -23,6 +27,8 @@ import mygame.state.gui.MainMenuGuiState;
 public final class MainMenuScreenState extends SagutAppState {
     
     private MainMenuGuiState guiMainMenu;
+    private GameCharacter character;
+    
     public MainMenuScreenState(SimpleApplication sapp) {
         super(sapp, "MainMenuScreen");
     }
@@ -37,8 +43,23 @@ public final class MainMenuScreenState extends SagutAppState {
         KeyBindings k = new KeyBindings();
         inputManager.addMapping("Start", new KeyTrigger(KeyInput.KEY_SPACE));
         inputManager.addListener(actionListener, "Start");
+        
+        // Adding kizuna
+        character = new GameCharacter(GameUtilities.getInstance().getAssetManager(), "Kizuna Ai", "Models/kizuna/kizuna.j3o");
+        character.createControl();
+        character.setCamera(camera);
+        character.setLocation(0, -3, 0);
+        character.addAndCreateAnimation("kizunaai_mesh");
+        character.setAnimation("Walk");
+        character.getSpatial().setLocalScale(2f);
+        localRootNode.attachChild(character.getSpatial());
+        
+        // Lighting
+        AmbientLight al=new AmbientLight();
+        al.setColor(new ColorRGBA(0.7f, 0.7f, 0.7f, 0.3f));
+        localRootNode.addLight(al);
+        sapp.getViewPort().setBackgroundColor(new ColorRGBA(0.8f, 0.8f, 0.8f, 1));
     }
-    
     private final ActionListener actionListener = new ActionListener() {
         protected boolean startDebouncer = false;
         @Override
@@ -51,10 +72,11 @@ public final class MainMenuScreenState extends SagutAppState {
     };
     
     @Override
-    protected void onDisable(){
+    protected void cleanup(Application app){
         inputManager.setCursorVisible(false);
         inputManager.removeListener(actionListener);
-        guiMainMenu.setEnabled(false);
+        this.stateManager.detach(guiMainMenu);
+        sapp.getViewPort().setBackgroundColor(ColorRGBA.Black);
     }
     
 }
